cba388ad646fa4947ba8e566a15e54b88e855848
matt <matt@matts-MacBook-Pro.local>
2016-11-13 18:45:45 -0800

    making queries

--- app/controllers/makequery_controller.rb
+++ app/controllers/makequery_controller.rb
@@ -3,13 +3,13 @@ class MakequeryController < ApplicationController
   def querysend
       @results = Array.new
       @connection = ActiveRecord::Base.connection
-      query = params[:query]
+      query = params[:q]
       result = @connection.exec_query(query)
       result.each do |row|
         array.push(row)
       end
       respond_to do |format|
-       format.json { render :json => @results }
-     end
+        format.json { render :json => @results }
+      end
   end
 end
