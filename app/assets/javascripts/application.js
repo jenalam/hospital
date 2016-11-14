75417742ce71df4fb4d94c61011cd31a65c7f8e9
Matt Hanna <matthanna19@hotmail.com>
2016-11-13 18:27:09 -0800

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,12 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+function execute(query) {
+  $.ajax({
+      url : "/makequery?query=" + query,
+      type : "GET",
+      async: false
+  });
+}
